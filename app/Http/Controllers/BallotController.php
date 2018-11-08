534cb6982f991b23d80802946e4f912f25f14c70
Ben Dailey <ben@daileyit.com>
2018-11-08 13:21:03 -0500

    add ballot db

---
+++ app/Http/Controllers/BallotController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Ballot;
+use Illuminate\Http\Request;
+
+class BallotController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Ballot  $ballot
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Ballot $ballot)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Ballot  $ballot
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Ballot $ballot)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Ballot  $ballot
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Ballot $ballot)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Ballot  $ballot
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Ballot $ballot)
+    {
+        //
+    }
+}
---
+++ database/migrations/2018_11_08_181251_create_ballots_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBallotsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ballots', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamp('open_at')->nullable();
+            $table->timestamp('close_at')->nullable();
+            $table->boolean('closed')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ballots');
+    }
+}
